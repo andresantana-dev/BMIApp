03129c5a593766eda7457e9a91c189a7277107a7
André Santana <andre.santana@polygon.pt>
2021-06-21 17:27:42 +0100

    add some refactor and add pop to view

--- BMIApp/View/ResultView.swift
+++ BMIApp/View/ResultView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ResultView: View {
     
+    @Environment(\.presentationMode) var presentation
+    
     init() {
         UITabBar.appearance().isHidden = true
         UINavigationBar.appearance().isHidden = true
@@ -19,33 +21,7 @@ struct ResultView: View {
             ZStack {
                 Color.white
                 
-                VStack(spacing: 10) {
-                    Text("Your BMI is")
-                        .foregroundColor(.black)
-                        .font(Font.custom("Gilroy-Bold", size: 40))
-                    
-                    Text("21.1")
-                        .foregroundColor(Constants.green)
-                        .font(Font.custom("Gilroy-Bold", size: 55))
-                    
-                    Text(
-                        "This value is in the normal range \nof 20.25 for your age group.\n"
-                            
-                            +
-                            
-                            "Keep up the good work!"
-                    )
-                    .font(Font.custom("Gilroy-Regular", size: 15))
-                    .multilineTextAlignment(.center)
-                    
-                    Image("person")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 200, height: 200)
-                        .offset(x: -30)
-                    
-                }
-                .padding()
+                InfoView()
                 
             }
             .frame(width: 320, height: 450)
@@ -56,6 +32,7 @@ struct ResultView: View {
             Spacer()
             
             Button(action: {
+                self.presentation.wrappedValue.dismiss()
             }, label: {
                 Text("Try Again")
                     .frame(width: 300, height: 50, alignment: .center)
@@ -74,5 +51,50 @@ struct ResultView: View {
 struct ResultView_Previews: PreviewProvider {
     static var previews: some View {
         ResultView()
+    }
+}
+
+struct InfoView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            Text("Your BMI is")
+                .foregroundColor(.black)
+                .font(Font.custom("Gilroy-Bold", size: 40))
+            
+            Text("21.1")
+                .foregroundColor(Constants.green)
+                .font(Font.custom("Gilroy-Bold", size: 55))
+            
+            Text(
+                "This value is in the normal range \nof 20.25 for your age group.\n"
+                    
+                    +
+                    
+                    "Keep up the good work!"
+            )
+            .font(Font.custom("Gilroy-Regular", size: 15))
+            .multilineTextAlignment(.center)
+            
+            HStack(spacing: -100) {
+                Image("person")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .offset(x: -30, y: 20)
+                
+                Image("message")
+                    .resizable()
+                    .frame(width: 100, height: 80)
+                    .offset(x: -60, y: -70)
+                    .overlay(
+                        Text("You're healthy!")
+                            .font(Font.custom("Gilroy-Regular", size: 11))
+                            .offset(x: -60, y: -80)
+                    )
+            }
+            .offset(y: 15)
+            
+        }
+        .padding()
     }
 }
